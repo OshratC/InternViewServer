c4273908d1f7bb29916babe4c39c2af71e180820
OshratC <oshratz2002@gmail.com>
2024-07-25 13:02:00 +0300

    add class

---
+++ InternViewServer/Models/Algotithem.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternViewServer.Models
+{
+    public class Algotithem
+    {
+    }
+}
